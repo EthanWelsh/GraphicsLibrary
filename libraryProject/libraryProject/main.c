62fd04b007e44b84a1613d58cd8a12b4f19735fe
welshej <welshej@gmail.com>
2014-09-13 20:42:30 -0400

    Added Library Shell

---
+++ libraryProject/libraryProject/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  libraryProject
+//
+//  Created by Ethan Welsh on 9/13/14.
+//  Copyright (c) 2014 Ethan Welsh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+
+    return 0;
+}
